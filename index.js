cf0db1ec8813148eb1dd5b7947d29a5bccbad81b
Satyajit Samal <samalsatyajit668@gmail.com>
2024-06-09 14:04:37 +0530

    Modified index.js

--- index.js
+++ index.js
@@ -2,22 +2,31 @@ const form = document.querySelector('form')
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
-
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
-    const results =  document.querySelector('#results')
+    const results = document.querySelector('#results')
     const weightGuide = document.querySelector('#weight-guide')
 
     if(height === '' || height < 0 || isNaN(height)) {
-        results.innerHTML = `Please enter a valid height ${height}`
+        const p = document.createElement('p')
+        p.appendChild(document.createTextNode(`Please enter a valid height ${height}`))
+        results.appendChild(p)
+        // results.innerHTML = `Please enter a valid height ${height}`
     } else if(weight === '' || weight < 0 || isNaN(weight)) {
-        results.innerHTML = `Please enter a valid weight ${weight}`
+        const p = document.createElement('p')
+        p.appendChild(document.createTextNode(`Enter a valid weight ${weight}`))
+        results.appendChild(p)
+        // results.innerHTML = `Enter a valid weight ${weight}`
+
     } else {
         const bmi = (weight / ((height * height)/10000)).toFixed(2)
-        results.innerHTML = `${bmi}`
+        const r = document.createElement('p')
+        r.appendChild(document.createTextNode(`${bmi}`))
+        results.appendChild(r)
+        // results.innerHTML = `${bmi}`
 
         const p = document.createElement('p')
-        if(bmi < 18.6)  {
+        if(bmi < 18.6) {
             p.appendChild(document.createTextNode(`The person is under weight`))
         } else if(bmi > 24.9) {
             p.appendChild(document.createTextNode(`The person is over weight`))
@@ -25,6 +34,7 @@ form.addEventListener('submit', function(e) {
             p.appendChild(document.createTextNode(`The person is normal ${bmi}`))
         }
         weightGuide.appendChild(p)
-
+        
     }
+
 })
